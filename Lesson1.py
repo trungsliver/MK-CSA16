3be07a496460117db680976f718dadb95fb1f21e
trungsliver <buiductrung.cva@gmail.com>
2024-11-03 20:54:16 +0700

    Lesson 1+2

---
+++ Lesson1.py
@@ -0,0 +1,53 @@
+# Nghiên cứu, phân tích dữ liệu của 1 shop thời trang để tăng doanh thu
+# Bước 1: Ask Question
+# Phỏng vấn nhân viên cửa hàng
+    # - Mặt hàng shop đang bán
+    # - Số lượng mặt hàng bán ra trong tháng (có thể dựa vào dữ liệu của cửa hàng)
+    # - Số lượng mặt hàng tồn kho (và các loại hàng tồn kho)
+    # - Sản phẩm bán chạy nhất
+# Phỏng vấn khách hàng
+    # - Lựa chọn sản phẩm của khách hàng
+    # - Tần suất mua hàng
+    # - Số tiền trung bình chi tiêu trong 1 lần mua sắm
+# ......
+
+# Bước 2: Tổng hợp thông tin
+# Bước 3: Phân tích dữ liệu
+    # - MẶt hàng bán chạy/ưa thích nhất => Đẩy mạnh quảng cáo/ sp tương tự
+    # - Số lượng mặt hàng bán ra trong tháng => Mục tiêu mới
+# ...
+# Bước 4: Trình bày kết quả
+
+# Kết quả tham khảo ptich quán cafe
+# Bước 1: Ask question - đặt câu hỏi từ các vấn đề có sẵn
+    # Đồ uống được gọi nhiều nhất => các mặt hàng và số lượng bán từng loại
+    # Khung giờ đông người nhất => thời gian mua hàng (lịch sử hóa đơn, chuyển khoản, camera)
+    # Cải thiện menu thế nào => loại bỏ các sản phẩm ít ng mua, bổ sung các sản phẩm tương tự các sản phẩm đang bán chạy,..
+
+# Bước 2: Thu thập và tìm kiếm dữ liệu
+    # Các mặt hàng: từ menu 
+    # Số lượng bán ra: hóa đơn, sản phẩm nhập - sản phẩm còn lại
+    # Thời gian mua hàng: lịch sử hóa đơn, chuyển khoản, camera
+    # Đối tượng mua hàng & sở thích: phỏng vấn nhân viên, check cam
+
+# Bước 3: Phân tích dữ liệu
+    # Số lượng bán ra: => những sản phẩm bán chạy, sản phẩm bán ế
+    # Doanh thu của từng loại đồ uống: => sản phẩm mang lại lợi nhuận cao nhất
+    # Thời gian mua hàng: => thời gian đông khách nhất
+    # Đối tượng mua hàng: => đối tượng khách hàng mua nhiều nhất
+    # => Sản phẩm đưuọc bán nhiều nhất trong khung giờ / ngày nào trong tuần
+
+# Bước 4: Rút ra kết luận
+    # Sản phẩm được gọi nhiều nhất / lợi nhuận cao nhất: => đẩy mạnh quảng cáo
+    # Sản phẩm ít bán được: loại bỏ khỏi menu
+    # Khung giờ đông người nhất: => tăng nhân viên, đẩy mạnh quảng cáo
+    # Chạy chương trình giảm giá ở các thời gian vắng khách
+
+
+# ===================================================
+# Phát biểu vấn đề (Problem Statement)
+# Câu hỏi thống kê (Statistical Question)
+    # Câu hỏi nghiên cứu/tra cứu (Research Question)
+    # Câu hỏi tính toán: 
+    # Câu hỏi liên hệ: 
+
