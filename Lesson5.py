80e2ae42d69fea6c7f3d25153921174efa551ec2
trungsliver <buiductrung.cva@gmail.com>
2024-12-06 13:42:36 +0700

    Lesson 5 - Checkpoint 1

---
+++ Lesson5.py
@@ -0,0 +1,13 @@
+# Link nộp bài
+# Gia Bình: https://colab.research.google.com/drive/1VtPQ_9XxENRwiYKf7sWz8QpRbJaBshN-?usp=sharing
+# Quang Huy:
+# Ngọc Nghi:
+# Việt Anh: https://colab.research.google.com/drive/1Fnn43IgPuvedeFTsJUWpLlEsb-aRNX-r?usp=sharing
+# Thành Đạt: https://colab.research.google.com/drive/1yX2LhdLkAi-2pRWninTJpS6WXZnZPh2X?usp=sharing
+
+# Kết quả
+# Gia Bình: tn 8/10, thực hành đúng 2 phần đầu, phần cuối sai TB 3/4 => 8.5
+# Quang Huy:
+# Ngọc Nghi:
+# Việt Anh: https://colab.research.google.com/drive/1Fnn43IgPuvedeFTsJUWpLlEsb-aRNX-r?usp=sharing
+# Thành Đạt: https://colab.research.google.com/drive/1yX2LhdLkAi-2pRWninTJpS6WXZnZPh2X?usp=sharing
